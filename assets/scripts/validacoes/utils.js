50e79475bb0e94b7194583146a6718fa4971a926
Matheus Rayol <matheusrbferreira@gmail.com>
2022-04-06 20:20:14 -0300

    Inclusão de novas funções globais no utils.js

--- assets/scripts/validacoes/utils.js
+++ assets/scripts/validacoes/utils.js
@@ -8,3 +8,26 @@ function converteValorRecebidoEmMinusculo (recebeValor) {
     return recebeValor.toLowerCase();
 }
 
+// Função para construção das mensagens de erro
+function constroiMensagemDeErro(mensagemDeErro, campoDeExibicao) {
+    campoDeExibicao.innerText = mensagemDeErro;
+    campoDeExibicao.style.fontSize = "8";
+    campoDeExibicao.style.fontWeight = "bold";
+    campoDeExibicao.style.color = "red";
+    campoDeExibicao.style.marginTop = "5px";
+}
+
+// Função para construção de mensagens informativas
+function constroiMensagemInformativa(mensagemInformativa, campoDeExibicao) {
+    campoDeExibicao.innerText = mensagemInformativa;
+    campoDeExibicao.style.fontSize = "8";
+    campoDeExibicao.style.fontWeight = "bold";
+    campoDeExibicao.style.color = "grey";
+    campoDeExibicao.style.marginTop = "5px";
+}
+
+// Função para limpar mensagem de erro
+function limpaMensagemDeErro(campoDeExibicao) {
+    campoDeExibicao.innerText = "";
+}
+
