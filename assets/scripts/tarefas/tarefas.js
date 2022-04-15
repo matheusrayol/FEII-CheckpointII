46f93c7c74e2652d1264611bb10d892428feafa4
Henrique <contatohenriquec@gmail.com>
2022-04-14 22:56:18 -0300

    Adicionar nome de usuário na área de tarefas

--- assets/scripts/tarefas/tarefas.js
+++ assets/scripts/tarefas/tarefas.js
@@ -15,7 +15,6 @@ onload = function () {
     if (tokenAtual()) {
         areaBloqueada.remove();
         areaConectada.removeAttribute('hidden');
-        startUp();
     } 
     // Se o token não existir, o usuário não está logado
     // Redireciona o usuário para a área de alerta
@@ -39,3 +38,51 @@ botaoSair.addEventListener('click', () =>
         location.href = 'index.html';
     });
 
+
+function exibeNome()
+{
+    if (tokenAtual())
+    {
+        const UrlEndpoint = "https://ctd-todo-api.herokuapp.com/v1/users/getMe";
+
+        let headerToken = new Headers();
+        headerToken.append('Authorization', tokenAtual())
+
+        console.log(headerToken)
+
+        const configuracaoDaRequisicao = {
+            method: 'GET',
+            headers: headerToken,
+            redirect: 'follow'
+        }
+
+        // Envia a requisição para a API de login
+        fetch(UrlEndpoint, configuracaoDaRequisicao).then(
+            // Recebe a resposta da API de login
+            resultado => {
+                // Verifica o status da resposta
+                // Se o status for 201, o login foi validado com sucesso
+                if (resultado.status == 200) {
+                    return resultado.json();
+                }
+                throw resultado;
+            }
+        )
+            .then(
+                // Recebe o objeto JSON da API de login
+                resultado => {
+                    // Chama a função sucessoNoLogin para continuar a requisição e armazenar o token no localStorage.
+                    campoUsername.innerText = 'Bem vindo, '+resultado.firstName; 
+                }
+            )
+            .catch(
+                erro => {
+                    // Caso o login não tenha sido bem-sucedido, informa a mensagem no console
+                    // e exibe a mensagem de erro abaixo do formulário de login.
+                    console.log(erro);
+                }
+            )
+    }
+}
+
+exibeNome();
